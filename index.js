bb94cb5d54ee2d593515742470b15e7d55a85619
Carlos Juarez Peña <isc20350597@gmail.com>
2023-09-20 09:59:50 -0600

    Query params

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express') //importando el módulo express
 
 const app = express()
+
+// http://localhost:3000/login?user=carlos&password=123
+
+app.get('/login', (req, res) => {
+    const {user,password}=req.query
+    if (!user || !password){
+        res.status(400).json({msg: 'You need to provide <user> and <password> parameters'})
+        return
+    }
+    if (user == 'carlos' && password == '123'){
+        res.json({msg: 'Iniciaste sesión con éxito'})
+        return
+    }
+    res.status(400).json("Error en el usuario o contraseña")
+})
+
+
+
 //http://localhost:3000/
 app.get('/:nombre', (req, res) => {
     const params = req.params
@@ -15,15 +33,11 @@ app.post('/', (req, res) => {
     res.json({msg: 'Hello GET'})
 })
 
-app.get('/:user/:pass', (req, res) => {
-    const {user,pass}=req.params
-    if (user == 'carlos' && pass == '123'){
-        res.json({msg: 'Iniciaste sesión con éxito'})
-    }
-    res.json("Error en el usuario o contraseña")
-})
 
-app.patch('/', (req, res) => {
+
+
+
+app.patch('/', (req, res) => { //Endpoint
     res.json("Hola con PATCH")
 })
 
