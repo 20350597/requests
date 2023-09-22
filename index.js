ad383e5a7494f5c46b94b7c91bb3320de87da81e
Carlos Juarez Peña <isc20350597@gmail.com>
2023-09-22 10:02:35 -0600

    Post

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express') //importando el módulo express
 
 const app = express()
-
+app.use(express.json())
 // http://localhost:3000/login?user=carlos&password=123
 
 app.get('/login', (req, res) => {
@@ -17,8 +17,6 @@ app.get('/login', (req, res) => {
     res.status(400).json("Error en el usuario o contraseña")
 })
 
-
-
 //http://localhost:3000/
 app.get('/:nombre', (req, res) => {
     const params = req.params
@@ -29,13 +27,22 @@ app.get('/:nombre', (req, res) => {
 
 
 /* Los navegadores sólo usan el método GET, por lo tanto los demás no se verán */
-app.post('/', (req, res) => {
-    res.json({msg: 'Hello GET'})
-})
 
+app.post('/login', (req, res) => {
+    const {user, password} = req.body
+    if (!user || !password){
+        res.status(400).json({msg: 'You need to provide <user> and <password> parameters'})
+    
+        return
+    }
+    if (user == 'carlos' && password == '123'){
+        res.json({msg: 'You sign in succesfully'})
+        return
 
-
-
+    res.status(404).json({
+        msg: 'Error en el usuario o contraseña'
+    })
+}});
 
 app.patch('/', (req, res) => { //Endpoint
     res.json("Hola con PATCH")
@@ -44,9 +51,6 @@ app.patch('/', (req, res) => { //Endpoint
 app.delete('/', (req, res) => {
     res.json("Hola con DELETE")
 })
-
-
-
 
 
 app.listen(3000, () => {console.log('listening on port 3000')})
